ce7471718a31c7b31b624e87c119a8db44fad73a
Rabiaparlak <68352029+Rabiaparlak@users.noreply.github.com>
2021-09-03 16:14:29 +0300

    first upload

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,14 @@
+@extends('admin.template')
+
+@section('icerik')
+
+Admin paneli index sayfası
+@endsection
+
+@section('css')
+
+@endsection
+
+@section('js')
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix'=>'yonetim'],function (){
+    Route::get('/',[\App\Http\Controllers\YonetimController::class,'index']);
+});
+    
+
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::group(['middleware'=>['auth','isAdmin'], 'prefix'=>'admin'],function(){
+
+
+});
